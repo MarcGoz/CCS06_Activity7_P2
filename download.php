5034e8abe56ddbd25557c1341bd759991c1f885a
MarcGoz <gozun.marccarlos@auf.edu.ph>
2023-04-26 20:45:04 +0800

    Part2

--- download.php
+++ download.php
@@ -41,14 +41,6 @@ try {
 
     fwrite($txt, $output);
     fclose($txt);
-
-    header('Content-Description: File Transfer');
-    header('Content-Disposition: attachment; filename=' . basename($file));
-    header('Expires: 0');
-    header('Cache-Control: must-revalidate');
-    header('Pragma: public');
-    header('Content-Length: ' . filesize($file));
-    header('Content-Type: text/plain');
     readfile($file);
     exit;
 
